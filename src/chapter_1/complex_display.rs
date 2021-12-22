9eaa7b92403dab9b05b07d95bc91b0edb79f7d5e
euberdeveloper <euberdeveloper@gmail.com>
2021-12-22 23:50:24 +0100

    feat: add first function of chapter 1
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ src/chapter_1/complex_display.rs
@@ -0,0 +1,22 @@
+use std::fmt::{self, Display, Formatter};
+
+pub fn run() {
+    let n = Complex {
+        real: 3.3,
+        imag: 7.2,
+    };
+    println!("The complex number is {}", n);
+    println!("{0:?} {:#?}", n);
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl Display for Complex {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{real} + {image}i", real = self.real, image = self.imag)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("CHAPTER 1");
+    println!("====================================");
+    
+    println!("Complex display");
+    println!("------------------------------------");
+    prova::chapter_1::complex_display::run();
+}
